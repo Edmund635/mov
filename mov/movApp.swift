a8ce7b6bca0a00b3b543c777d0b7383aeee54ddd
Edmund <edmund.adjei635@gmail.com>
2023-01-21 13:15:26 -0500

    Initial Commit

---
+++ mov/movApp.swift
@@ -0,0 +1,17 @@
+//
+//  movApp.swift
+//  mov
+//
+//  Created by Edmund Adjei on 1/21/23.
+//
+
+import SwiftUI
+
+@main
+struct movApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ movUITests/movUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  movUITestsLaunchTests.swift
+//  movUITests
+//
+//  Created by Edmund Adjei on 1/21/23.
+//
+
+import XCTest
+
+final class movUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
